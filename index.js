497e4107ad7fde524af8e44913fd01f52a75d0f0
Evgeniy Sheleshkov <e.sheleshkov@gmail.com>
2019-07-03 13:48:29 +0300

    Добавление onMapClick

--- index.js
+++ index.js
@@ -3,5 +3,22 @@ const init = () => {
         center: [59.91807704, 30.30489950],
         zoom: 14
     });
+
+    const onMapClick = (event) => {
+        var coords = event.get('coords');
+        myMap.balloon.open(coords, {
+            contentHeader: 'Событие', 
+            contentBody: '<p>Кто-то щелкнул по карте.</p>' +
+                '<p>Координаты щелчка: ' + [
+                coords[0].toPrecision(6),
+                coords[1].toPrecision(6)
+                ].join(', ') + '</p>',
+            contentFooter:'<sup>Щелкните еще раз</sup>'
+        });
+        //console.log(event.target);
+    };
+
+    myMap.events.add('click', onMapClick);
+
 };
 ymaps.ready(init);
